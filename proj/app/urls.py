55d833fab60dc44ad0b87e24ee44a93ff9e575f8
Ameya Potdar <ameyapradippotdar@gmail.com>
2023-11-13 12:09:17 +0530

    uploading files to django

---
+++ proj/app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('image', views.image),
+    path('upload', views.upload),
+]
+
+if settings.DEBUG:
+
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ proj/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def image(request):
+    pass
+
+def upload(request):
+    return render(request,'upload.html')
